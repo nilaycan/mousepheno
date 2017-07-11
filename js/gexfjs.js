3df32753b31c797cf028ba468512458b411a6cdb
nilaycan <cannilay@gmail.com>
2017-07-11 17:03:45 +0300

    “aciklama

--- js/gexfjs.js
+++ js/gexfjs.js
@@ -256,9 +256,9 @@ function replaceURLWithHyperlinks(text) {
 }
 
 function displayNode(_nodeIndex, _recentre) {
+    GexfJS.params.currentNode = _nodeIndex;
     document.getElementById("exportlink").style.visibility = "visible";
     document.getElementById("exportpng").style.visibility = "visible";
-    GexfJS.params.currentNode = _nodeIndex;
     if (_nodeIndex != -1) {
         var _d = GexfJS.graph.nodeList[_nodeIndex],
             _b = _d.coords.base,
@@ -1208,74 +1208,50 @@ $(document).ready(function() {
     });
     $("a#exportlink").click(function(){
         var items = $($('#leftcontent ul')[1])[0]['children'];
+       // var display = 'Nodes\n';
         var gene = $('#leftcontent h3').text();
         if (gene.indexOf(":") >= 0) {
              var res = gene.split(":");
              var display = res[1].substring(1)+'\n';
         }
         else{
-            var display = gene + '\n';
+           
+            //var display = GexfJS.params.showEdgeWeight + '\n';
+           var display = gene + '\n';
         }
-
-       for (var i = 0; i < items.length; i++) {
+       
+        //
+        for (var i = 0; i < items.length; i++) {
             if (items[i]['innerText'].indexOf("[") >= 0) {
              var res = items[i]['innerText'].split("[");
              var name = res[0];
              var res2 = res[1].split("]");
              var weight = res2[0];
              display += name + '\t' + weight + '\n';
-        }
-        else{          
-            var name = items[i]['innerText'];
-             var weight = 1;
-             display += name + '\t' + weight + '\n';
-        }
-        }
-        this.href = 'data:application/vnd.ms-excel,' + encodeURIComponent(display);
-    });
-
-    $("a#exportpng").click(function(e){
-        var canvasElement = document.getElementById('carte');
-        var MIME_TYPE = "image/png";
-        var imgURL = canvasElement.toDataURL(MIME_TYPE);
-        this.href = imgURL;
-        this.dataset.downloadurl = [MIME_TYPE, this.download, this.href].join(':');
-    });
-});
-
-$("a#exportlink").click(function(){
-        var items = $($('#leftcontent ul')[1])[0]['children'];
-        var gene = $('#leftcontent h3').text();
-        if (gene.indexOf(":") >= 0) {
-             var res = gene.split(":");
-             var display = res[1].substring(1)+'\t Edge Weight'+'\n';
         }
         else{
-            var display = gene+'\t Edge Weight' + '\n';
-        }
-
-       for (var i = 0; i < items.length; i++) {
-            if (items[i]['innerText'].indexOf("[") >= 0) {
-             var res = items[i]['innerText'].split("[");
-             var name = res[0];
-             var res2 = res[1].split("]");
-             var weight = res2[0];
-             display += name + '\t' + weight + '\n';
-        }
-        else{          
+           
             var name = items[i]['innerText'];
              var weight = 1;
              display += name + '\t' + weight + '\n';
         }
+            //display += items[i]['innerText'] + '\t' + 'asd' + '\n';
         }
-        this.href = 'data:application/vnd.ms-excel,' + encodeURIComponent(display);
+        //alert(display);
+        //display = display.replace(/ /g, '%20');
+        this.href = 'data:application/vnd.ms-excel,' + encodeURIComponent(display);;
+        //a.download = 'exported_nodes.txt';
+    //var now = new Date().toString();
+    //this.href = "data:text/plain;charset=UTF-8,"  + encodeURIComponent(now);
     });
-
     $("a#exportpng").click(function(e){
+        //e.preventDefault();
         var canvasElement = document.getElementById('carte');
+        //console.log(canvasElement);
         var MIME_TYPE = "image/png";
         var imgURL = canvasElement.toDataURL(MIME_TYPE);
         this.href = imgURL;
         this.dataset.downloadurl = [MIME_TYPE, this.download, this.href].join(':');
+        //this.click();
     });
 });
